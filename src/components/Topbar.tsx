883aa9eaf043efce9a264b2417c7304fb47533ad
Arcofoods-mkt <leyses.mesquita@arcofoods.com.br>
2026-07-22 16:31:18 -0300

    1

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -22,9 +22,9 @@ export default function Topbar() {
     <>
       <header className="topbar container">
         <div style={{ display: 'flex', alignItems: 'center', gap: '1.5rem' }}>
-          <Image src="/imagens/arcowsvg.svg" alt="Arcofoods Logo" width={200} height={55} className="logo-img" unoptimized />
+          <Image src="/Imagens/arcowsvg.svg" alt="Arcofoods Logo" width={200} height={55} className="logo-img" unoptimized />
           <div style={{ width: '1px', height: '35px', backgroundColor: 'var(--glass-border)' }}></div>
-          <Image src="/imagens/jotajasummit.svg" alt="JotaJá Summit Logo" width={200} height={45} style={{ height: '45px', width: 'auto', flexShrink: 0 }} unoptimized />
+          <Image src="/Imagens/jotajasummit.svg" alt="JotaJá Summit Logo" width={200} height={45} style={{ height: '45px', width: 'auto', flexShrink: 0 }} unoptimized />
         </div>
 
         {/* Navegação Desktop */}
